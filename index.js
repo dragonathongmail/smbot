8e07b9f1e317a45adbe81c7813f9666f96468734
Long Huynh <longhb@smartosc.com>
2017-10-04 15:55:30 +0700

    Commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const Hapi = require('hapi');
 const server = new Hapi.Server();
 server.connection({
     host: 'localhost',
-    port: 8080
+    port: process.env.PORT || 8080
 });
 
 // Add the route
