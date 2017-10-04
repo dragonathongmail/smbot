06c6af1c5c1fe74776cc3f1615b74824f68e3d75
Long Huynh <longhb@smartosc.com>
2017-10-04 16:15:22 +0700

    Commit

--- index.js
+++ index.js
@@ -1,42 +1,18 @@
-'use strict';
-const fs = require('fs');
+var express = require('express');
+var app = express();
 
-// The rest of the code implements the routes for our Express server.
-var privateKey  = fs.readFileSync('key.pem', 'utf8');
-var certificate = fs.readFileSync('cert.pem', 'utf8');
+app.set('port', (process.env.PORT || 5000));
 
-var credentials = {key: privateKey, cert: certificate};
-const Hapi = require('hapi');
+app.use(express.static(__dirname + '/public'));
 
-// Create a server with a host and port
-const server = new Hapi.Server();
-server.connection({
-    host: 'localhost',
-    port: process.env.PORT || 8080
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
 
-// Add the route
-server.route({
-    method: 'GET',
-    path:'/webhook',
-    handler: function (request, reply) {
-        if (request.query['hub.mode'] === 'subscribe' &&
-            request.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
-            console.log("Validating webhook");
-            reply(request.query['hub.challenge']).code(200);
-        } else {
-            console.error("Failed validation. Make sure the validation tokens match.");
-            reply("Failed validation").code(403);
-        }
-    }
-});
-
-// Start the server
-server.start((err) => {
-
-    if (err) {
-        throw err;
-    }
-    console.log('Server running at:', server.info.uri);
-    console.log(process.env);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
