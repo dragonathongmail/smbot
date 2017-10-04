73be1f0a2d6645b61b3beeabc6188ff75a913966
Long Huynh <longhb@smartosc.com>
2017-10-04 16:20:41 +0700

    Commit

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
+//
+// app.set('port', (process.env.PORT || 5000));
+//
+// app.use(express.static(__dirname + '/public'));
+//
+// // views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+//
+// app.get('/', function(request, response) {
+//   response.render('pages/index');
+// });
+//
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
 
-app.set('port', (process.env.PORT || 5000));
+'use strict';
+const Hapi = require('hapi');
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+// Create a server with a host and port
+const server = new Hapi.Server();
+server.connection({
+    host: 'botttt.herokuapp.com',
+    port: process.env.PORT || 5000
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+// Add the route
+server.route({
+    method: 'GET',
+    path:'/webhook',
+    handler: function (request, reply) {
+        if (request.query['hub.mode'] === 'subscribe' &&
+            request.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
+            console.log("Validating webhook");
+            reply(request.query['hub.challenge']).code(200);
+        } else {
+            console.error("Failed validation. Make sure the validation tokens match.");
+            reply("Failed validation").code(403);
+        }
+    }
+});
+
+// Start the server
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
 });
